b4dfced4d1ba051deaa1562d271890240f684a37
Sam Wigley <swigley@tistatech.com>
2020-12-08 16:43:47 -0600

    added radar chart component

---
+++ src/app/radar-chart/radar-chart.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
+@Component({
+  selector: 'app-radar-chart',
+  templateUrl: './radar-chart.component.html',
+  styleUrls: ['./radar-chart.component.scss']
+})
+export class RadarChartComponent implements OnInit {
+  public radarChartLabels = ['Python', 'SQL', 'AWS', 'Apache Spark', 'Machine Learning', 'Angular.js',];
+  public radarChartData = [
+    {data: [100, 120, 90, 70, 42, 28], label: 'You'},
+    {data: [125, 150, 77, 98, 130, 88], label: 'This Job'}
+  ];
+  public radarChartType = 'radar';
+  public radarChartOptions: RadialChartOptions = {
+    responsive: false,
+  };
+  constructor() { }
+  ngOnInit() {
+  }
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+}
