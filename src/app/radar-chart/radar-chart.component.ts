196e33e121354dfb4c3fe5d2ea23762465ce08a7
Sam Wigley <swigley@tistatech.com>
2020-12-16 11:28:58 -0600

    updated chart component

--- src/app/radar-chart/radar-chart.component.ts
+++ src/app/radar-chart/radar-chart.component.ts
@@ -6,17 +6,28 @@ import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
   styleUrls: ['./radar-chart.component.scss']
 })
 export class RadarChartComponent implements OnInit {
+
   public radarChartLabels = ['Python', 'SQL', 'AWS', 'Apache Spark', 'Machine Learning', 'Angular.js',];
   public radarChartData = [
     {data: [100, 120, 90, 70, 42, 28], label: 'You'},
-    {data: [125, 150, 77, 98, 130, 88], label: 'This Job'}
+    {data: [85 + this.getRandomInt(50),
+        10 + this.getRandomInt(50),
+        27 + this.getRandomInt(50),
+        38 + this.getRandomInt(50),
+        30 + this.getRandomInt(50),
+        58 + this.getRandomInt(50)], label: 'This Job'}
   ];
   public radarChartType = 'radar';
   public radarChartOptions: RadialChartOptions = {
     responsive: false,
   };
+  public width = 250;
+  public height = 250;
   constructor() { }
   ngOnInit(): void {
+  }
+  getRandomInt(max): number {
+    return Math.floor(Math.random() * Math.floor(max));
   }
   public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
@@ -25,4 +36,5 @@ export class RadarChartComponent implements OnInit {
   public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
   }
+
 }
